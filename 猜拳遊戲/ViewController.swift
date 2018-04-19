7c6690b6ec195f9a5183136bcbc9c04bff780977
“Wen0926” <chienwen0926@gmail.com>
2018-04-19 13:54:17 +0800

    Initial Commit

---
+++ 猜拳遊戲/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  猜拳遊戲
+//
+//  Created by ChienWen on 2018/4/19.
+//  Copyright © 2018年 ChienWen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
